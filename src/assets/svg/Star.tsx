7a30819295ab264cd312a12f0c17200a01f2a260
sahar mohammadi <saharmohammadi0224@gmail.com>
2025-02-05 14:35:46 +0330

    add assets

---
+++ src/assets/svg/Star.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Star() {
+    return (
+<svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width="19px" height="19px">
+    <path
+        d="M19.467,23.316,12,17.828,4.533,23.316,7.4,14.453-.063,9H9.151L12,.122,14.849,9h9.213L16.6,14.453Z" fill="#133e87" />
+</svg>
+
+    );
+}
