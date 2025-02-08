1086a3d4056f3c6cddd30685676e22ed5700ca9d
sahar mohammadi <saharmohammadi0224@gmail.com>
2025-02-08 15:37:36 +0330

    Update api.tsx

--- src/server/api.tsx
+++ src/server/api.tsx
@@ -1,27 +1,46 @@
-// برای درخواست GET
+
+
+
+const BASE_URL = "http://127.0.0.1:8000/";
+
 export const fetchProducts = async (url: string) => {
     try {
-        const response = await fetch("http://192.168.110.104:5000"+ url);
+        const response = await fetch(`${BASE_URL}${url}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                // "Authorization": `Bearer ${yourToken}`  // در صورت نیاز به توکن
+            }
+        });
+
         if (!response.ok) {
-            throw new Error("Failed to fetch products");
+            const errorMessage = await response.text(); // دریافت پیام خطای سرور
+            throw new Error(`خطا در دریافت اطلاعات: ${errorMessage}`);
         }
-        const data = await response.json();
-        return data; 
+
+        return await response.json();
     } catch (error) {
-        console.error("Error fetching products:", error);
-        throw error; 
+        console.error("خطا در دریافت محصولات:", error);
+        throw error;
     }
 };
 
 
+
+
 // برای درخواست POST
 export const submitForm = async (url: string, formData: object) => {
+const BASE_URL = "http://127.0.0.1:8000/";
+
     try {
-        const response = await fetch("http://localhost:8000/"+ url, {
+        // const csrfToken = getCsrfToken();
+        const response = await fetch(new URL(url, BASE_URL).toString(),{
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
+                // "X-CSRFToken": csrfToken|| "", 
             },
+            // credentials: "include",  
             body: JSON.stringify(formData),
         });
  
